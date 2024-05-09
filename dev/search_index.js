df9d491c957568b3643bed777dd9cba4ea407be9
Documenter.jl <documenter@juliadocs.github.io>
2024-05-09 16:40:05 +0000

    build based on 508726d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Shapes","category":"page"},{"location":"#Shapes","page":"Home","title":"Shapes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Shapes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Shapes]","category":"page"}]
+}
